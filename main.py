c1649a676484eb74595b13d0043dec3caff6d903
stark-1110 <s2446031@stu.musashino-u.ac.jp>
2026-09-13 16:25:39 +0900

    Update main.py

--- main.py
+++ main.py
@@ -160,8 +160,8 @@ async def main():
                         server.quit()
                         print(f"✉️ ({len(TO_EMAIL)}名) へメールを送信しました！")
                         
-                        except Exception as mail_err:
-                            print(f"❌ メール送信エラー ({type(mail_err).__name__}): {mail_err}")
+                    except Exception as mail_err:
+                        print(f"❌ メール送信エラー ({type(mail_err).__name__}): {mail_err}")
 
                 else:
                     print("空きがすべて埋まったため、メールはスキップします。")
